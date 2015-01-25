b4a1319994dc7d8f06a3818b68c721054ca8970c
Ranieri Althoff <ranisalt@gmail.com>
2015-01-25 20:43:06 -0200

    Make tests be run by runscript

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
+import argparse
 from app import app
 
-app.run()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--test', help='run tests only', action='store_true', dest='test', default=False)
+    args = parser.parse_args()
+
+    if args.test:
+        from app import tests
+        tests.run_tests()
+
+    else:
+        app.run()
