f3228cbc552cc298c55b889e22dcdd3cb956cb7a
Ranieri Althoff <ranisalt@gmail.com>
2015-01-26 19:33:18 -0200

    Handle exception on database connection refused

--- app/moita.py
+++ app/moita.py
@@ -1,10 +1,19 @@
 import config
+import errno
 import flask
 import pymongo
+import pymongo.errors
+import sys
 
 app = flask.Flask(__name__)
 app.config.from_object(config)
 
-connection = pymongo.MongoClient()
+try:
+    connection = pymongo.MongoClient()
+except pymongo.errors.ConnectionFailure:
+    print('Error %d: connection to the database refused.' % errno.ECONNREFUSED, file=sys.stderr)
+    sys.exit(errno.ECONNREFUSED)
+
+
 database = connection[app.config['DATABASE']]
 timetables = database.timetables
