ac6a7cd897c7425e7d3bf201fcba139c68d043cb
Ranieri Althoff <ranisalt@gmail.com>
2015-11-25 01:40:10 -0200

    Implement integration with Amazon S3, update deps

--- app/moita.py
+++ app/moita.py
@@ -2,12 +2,38 @@ import config
 import errno
 import sys
 
+import boto
+import boto.s3.connection as s3connection
+import boto.s3.key as s3key
 import flask
+import json
 
 app = flask.Flask(__name__)
 app.config.from_object(config)
 
 map = flask.Blueprint('moita', __name__)
+
+s3 = s3connection.S3Connection()
+bucket = s3.get_bucket(app.config['AWS_BUCKET_NAME'])
+
+
+def download(filename):
+    key = s3key.Key(bucket)
+    key.key = '%s.json' % (filename,)
+    filedata = key.get_contents_as_string()
+    try:
+        return json.loads(filedata.decode('utf-8'))
+    except UnicodeError:
+        return None
+
+
+def upload(filename, filedata):
+    key = s3key.Key(bucket)
+    key.key = '%s.json' % (filename,)
+    key.set_contents_from_string(json.dumps(filedata), headers={
+            'Content-Type': 'application/json',
+    })
+    return key
 
 
 @map.route('/load/<identifier>', methods=['GET'])
--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 APPLICATION_ROOT = '/moita'
 DATABASE = 'moita'
+
+# Amazon S3 options
+AWS_BUCKET_NAME = 'matrufsc'
