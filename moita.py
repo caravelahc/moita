485051d7cf983684be29e200890b5c4b0c8898d3
Barichello <artur@barichello.me>
2021-10-02 16:01:13 -0300

    Format using black

--- moita.py
+++ moita.py
@@ -5,58 +5,64 @@ from textwrap import dedent
 
 
 def initialize_db():
-    db = sqlite3.connect('matrufsc.db')
+    db = sqlite3.connect("matrufsc.db")
     db.execute(
-        dedent('''\
+        dedent(
+            """\
             create table if not exists schedules (
                 id text primary key,
                 json_data text
             );
-        ''')
+        """
+        )
     )
 
 
-map = flask.Blueprint('moita', __name__)
+map = flask.Blueprint("moita", __name__)
 
 
-@map.route('/load/<identifier>', methods=['GET'])
+@map.route("/load/<identifier>", methods=["GET"])
 def load_timetable(identifier):
-    with sqlite3.connect('matrufsc.db') as db:
-        query = dedent('''\
+    with sqlite3.connect("matrufsc.db") as db:
+        query = dedent(
+            """\
             select json_data
             from schedules
             where id = ?
-        ''')
-        result, *_ = db.execute(query, (identifier, ))
+        """
+        )
+        result, *_ = db.execute(query, (identifier,))
 
     return result[0], 200
 
 
-@map.route('/store/<identifier>', methods=['PUT'])
+@map.route("/store/<identifier>", methods=["PUT"])
 def store_timetable(identifier):
     data = flask.request.get_json()
 
-    with sqlite3.connect('matrufsc.db') as db:
+    with sqlite3.connect("matrufsc.db") as db:
         db.execute(
-            dedent('''\
+            dedent(
+                """\
                 insert or replace into schedules(id, json_data)
                 values (?, ?)
-            '''),
-            (identifier, str(data))
+            """
+            ),
+            (identifier, str(data)),
         )
 
-    return '', 204
+    return "", 204
 
 
-@map.route('/ical/<identifier>', methods=['POST'])
+@map.route("/ical/<identifier>", methods=["POST"])
 def reflect_ical(identifier):
     # data comes as bytes and split, needs to be decoded then joined
-    data = ''.join(flask.request.get_data().decode('UTF-8'))
+    data = "".join(flask.request.get_data().decode("UTF-8"))
 
     response = flask.make_response(data)
     headers = {
-        'Content-Type': 'text/calendar',
-        'Content-Disposition': 'attachment; filename=%s.ics' % (identifier, ),
+        "Content-Type": "text/calendar",
+        "Content-Disposition": "attachment; filename=%s.ics" % (identifier,),
     }
 
     for k in headers:
@@ -73,7 +79,7 @@ def create_app(**kwargs):
     app.config.from_object(config)
     app.config.update(kwargs)
 
-    app.register_blueprint(map, url_prefix=app.config.get('APPLICATION_ROOT'))
+    app.register_blueprint(map, url_prefix=app.config.get("APPLICATION_ROOT"))
 
     initialize_db()
 
